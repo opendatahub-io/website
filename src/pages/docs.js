d9ab5dcf04df8aee5f213606b443d7127039f256
Christopher Chase <cchase@redhat.com>
2020-08-24 13:00:42 -0400

    Revert "OpenDataHub Migration using Gatsby"

--- src/pages/docs.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import DocsLayout from '../components/docs_layout'
-import '../styles/_docs.scss'
-
-const Docs = () =>
-<DocsLayout>
-</DocsLayout>   
-
-export default Docs
